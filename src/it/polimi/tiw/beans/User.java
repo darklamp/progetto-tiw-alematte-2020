8e3357eee0d8beefe3882edd738a3795ebc3b2ed
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-04-11 09:34:51 +0200

    Completed User class and UserDAO for login actions

--- src/it/polimi/tiw/beans/User.java
+++ src/it/polimi/tiw/beans/User.java
@@ -1,4 +1,62 @@
 package it.polimi.tiw.beans;
 
 public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String role;
+    private String level = null;
+    private String imageURL = null;
+
+    public User() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
