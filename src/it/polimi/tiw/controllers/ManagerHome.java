cd1be42f8510daf9a7d3dd972cc8241f3376f4e7
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-04-11 15:28:54 +0200

    Add filters and worker, manager home servlet classes

---
+++ src/it/polimi/tiw/controllers/ManagerHome.java
@@ -0,0 +1,9 @@
+package it.polimi.tiw.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/manager")
+public class ManagerHome extends HttpServlet {
+
+}
