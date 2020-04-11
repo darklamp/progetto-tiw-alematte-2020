4d8034904a2c145100ae6ff38b84810235a16d23
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-04-11 19:05:34 +0200

    Add CampaignDetail class and template

---
+++ src/it/polimi/tiw/controllers/CampaignDetail.java
@@ -0,0 +1,80 @@
+package it.polimi.tiw.controllers;
+
+import it.polimi.tiw.beans.Campaign;
+import it.polimi.tiw.dao.CampaignDAO;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@WebServlet("/manager/campaign/*")
+public class CampaignDetail extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private Connection connection = null;
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext context = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+
+        try{
+            String driver = context.getInitParameter("dbDriver");
+            String url = context.getInitParameter("dbUrl");
+            String user = context.getInitParameter("dbUser");
+            String password = context.getInitParameter("dbPassword");
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new UnavailableException("Can't load database driver");
+        } catch (SQLException e) {
+            throw new UnavailableException("Couldn't get db connection");
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int campaignId = Integer.parseInt(req.getParameter("id"));
+
+        String path = "/WEB-INF/campaignDetail.html";
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
+
+        CampaignDAO campaignDAO = new CampaignDAO(connection);
+        Campaign campaign = null;
+        try{
+            campaign = campaignDAO.getCampaignById(campaignId);
+        } catch (SQLException e){
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        if(campaign==null){
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        //Classes are not null
+
+        ctx.setVariable("campaign", campaign);
+        templateEngine.process(path, ctx, resp.getWriter());
+    }
+}
