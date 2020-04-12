6cbdbd8af5e4d0549c614c58bea45a3ec568fd06
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-04-12 10:07:59 +0200

    Fixed html multipart form declaration in register and completed register servlet file upload

--- src/it/polimi/tiw/controllers/Register.java
+++ src/it/polimi/tiw/controllers/Register.java
@@ -1,7 +1,6 @@
 package it.polimi.tiw.controllers;
 
 import it.polimi.tiw.beans.Alert;
-import it.polimi.tiw.beans.User;
 import it.polimi.tiw.dao.UserDAO;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -11,21 +10,29 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @WebServlet("/register")
+@MultipartConfig(fileSizeThreshold = 6291456, // 6 MB
+        maxFileSize = 10485760L, // 10 MB
+        maxRequestSize = 20971520L // 20 MB
+)
 public class Register extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private Connection connection = null;
     private TemplateEngine templateEngine;
+    private static final String UPLOAD_DIR = "uploads/profileImages";
 
     @Override
     public void init() throws ServletException {
@@ -115,13 +122,51 @@ public class Register extends HttpServlet {
                 return;
             }
 
+            if(role.equals("manager")){
+                //No image and level
+                userDAO.addManagerUser(username, email, password, role);
+            } else {
+                //Get photo and level
+                String experience = req.getParameter("experience");
+                if(!experience.equals("low") && !experience.equals("medium") && !experience.equals("high")){
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid experience");
+                    return;
+                }
 
+                // gets absolute path of the web application
+                String applicationPath = req.getServletContext().getRealPath("");
+                // constructs path of the directory to save uploaded file
+                String uploadFilePath = applicationPath + File.separator + UPLOAD_DIR;
+                // creates upload folder if it does not exists
+                File uploadFolder = new File(uploadFilePath);
+                if (!uploadFolder.exists()) {
+                    uploadFolder.mkdirs();
+                }
+                Part part = req.getPart("photo");
+                if(part != null && part.getSize()>0){
+                    //File exists
+                    String fileName = part.getSubmittedFileName();
+                    String contentType = part.getContentType();
+                    String savedFileName = username + getFileExtension(fileName);
+                    // allows only JPEG and PNG files to be uploaded
+                    if (!contentType.equalsIgnoreCase("image/jpeg") && !contentType.equalsIgnoreCase("image/png")) {
+                        alert.setType(Alert.DANGER);
+                        alert.setContent("Wrong file type");
+                        alert.show();
+                        resp.sendRedirect(getServletContext().getContextPath() + "/register");
+                        return;
+                    }
+                    part.write(uploadFilePath + File.separator + fileName);
+                    userDAO.addWorkerUser(username, email, password, role, experience, savedFileName);
+                } else {
+                    userDAO.addWorkerUser(username, email, password, role, experience, null);
+                }
 
-            userDAO.addUser(username, email, password, role);
+            }
 
         } catch (SQLException e){
             alert.setType(Alert.DANGER);
-            alert.setContent("Database or SQL error");
+            alert.setContent("Database or SQL error" + e.getMessage());
             alert.show();
             resp.sendRedirect(getServletContext().getContextPath() + "/register");
             return;
@@ -144,5 +189,11 @@ public class Register extends HttpServlet {
             }
         } catch (SQLException sqle) {
         }
+    }
+
+    private static String getFileExtension(String fileName) {
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".")+1);
+        else return "";
     }
 }
