a3a576558f09f870bd37dd896e228510492ae739
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-04-12 19:04:35 +0200

    Add profile templete and started Profile class

--- src/it/polimi/tiw/controllers/Profile.java
+++ src/it/polimi/tiw/controllers/Profile.java
@@ -1,4 +1,97 @@
 package it.polimi.tiw.controllers;
 
-public class Profile {
+import it.polimi.tiw.beans.Alert;
+import it.polimi.tiw.beans.User;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/profile")
+public class Profile extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private Connection connection = null;
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext context = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+
+        try{
+            String driver = context.getInitParameter("dbDriver");
+            String url = context.getInitParameter("dbUrl");
+            String user = context.getInitParameter("dbUser");
+            String password = context.getInitParameter("dbPassword");
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new UnavailableException("Can't load database driver");
+        } catch (SQLException e) {
+            throw new UnavailableException("Couldn't get db connection");
+        }
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Show page
+        String path;
+        User user = (User) req.getSession().getAttribute("user");
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
+        Alert profileAlert;
+        if(req.getSession().getAttribute("profileAlert")==null){
+            profileAlert = new Alert(false, Alert.DANGER, "");
+            req.getSession().setAttribute("profileAlert", profileAlert);
+        } else {
+            profileAlert = (Alert) req.getSession().getAttribute("profileAlert");
+        }
+
+        if(user.getRole().equals("worker")){
+            //Load worker profile template
+            path = "/WEB-INF/profile/workerProfile.html";
+        }else if(user.getRole().equals("manager")){
+            //Load manager profile templete
+            path = "/WEB-INF/profile/managerProfile.html";
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        ctx.setVariable("profileAlert", req.getSession().getAttribute("profileAlert"));
+        ctx.setVariable("user", user);
+        templateEngine.process(path, ctx, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       //Update information and then resp.sendRedirect(getServletContext().getContextPath()+"/profile");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException sqle) {
+        }
+    }
 }
