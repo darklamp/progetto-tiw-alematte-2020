07478431c73551302b817919bc055b5fce892943
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-04-13 18:20:29 +0200

    Add CampaignReport class and template

---
+++ src/it/polimi/tiw/controllers/CampaignReport.java
@@ -0,0 +1,9 @@
+package it.polimi.tiw.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/manager/campaignReport")
+public class CampaignReport extends HttpServlet {
+
+}
