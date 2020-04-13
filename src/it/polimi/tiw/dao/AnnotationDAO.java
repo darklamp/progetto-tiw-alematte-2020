8cb55fba3fa65b0cc9bb5ca1c5bc77dbb1c30303
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-04-13 10:22:53 +0200

    Created JSON servlet to get image data

---
+++ src/it/polimi/tiw/dao/AnnotationDAO.java
@@ -0,0 +1,69 @@
+package it.polimi.tiw.dao;
+
+import it.polimi.tiw.beans.Annotation;
+import it.polimi.tiw.beans.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnnotationDAO {
+    private Connection connection;
+
+    public AnnotationDAO(Connection connection){
+        this.connection = connection;
+    }
+
+    public void createAnnotation(int workerId, int imageId, int validity, String trust, String note){
+
+    }
+
+    public Annotation getAnnotation(int workerId, int imageId) throws SQLException {
+        String query = "SELECT * FROM annotation WHERE workerId = ? AND imageId = ?";
+        try (PreparedStatement pstatement = connection.prepareStatement(query);) {
+            pstatement.setInt(1, workerId);
+            pstatement.setInt(2, imageId);
+            try (ResultSet result = pstatement.executeQuery();) {
+                if (!result.isBeforeFirst()) // no results, credential check failed
+                    return null;
+                else {
+                    result.next();
+                    Annotation annotation = new Annotation();
+                    annotation.setWorkerId(workerId);
+                    annotation.setImageId(imageId);
+                    annotation.setDate(result.getDate("date"));
+                    annotation.setValidity(result.getInt("validity"));
+                    annotation.setTrust(result.getString("trust"));
+                    annotation.setNote(result.getString("note"));
+                    return annotation;
+                }
+            }
+        }
+    }
+
+    public List<Annotation> getAnnotations(int imageId) throws SQLException{
+        List<Annotation> annotations = new ArrayList<>();
+        String query = "SELECT * FROM annotation WHERE imageId = ?";
+        try (PreparedStatement pstatement = connection.prepareStatement(query);) {
+            pstatement.setInt(1, imageId);
+            try (ResultSet result = pstatement.executeQuery();) {
+                while(result.next()){
+                    Annotation annotation = new Annotation();
+                    annotation.setWorkerId(result.getInt("workerId"));
+                    annotation.setImageId(imageId);
+                    annotation.setDate(result.getDate("date"));
+                    annotation.setValidity(result.getInt("validity"));
+                    annotation.setTrust(result.getString("trust"));
+                    annotation.setNote(result.getString("note"));
+                    annotations.add(annotation);
+                }
+                return annotations;
+            }
+        }
+    }
+
+
+}
