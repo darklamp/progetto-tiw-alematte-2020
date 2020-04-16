c5160dc04ec61ad450c29742e562d2fa81b9b13c
Alessandro Villa <alessandro17.villa@mail.polimi.it>
2020-04-16 13:57:22 +0200

    Fix for closed campaigns
    Signed-off-by: Alessandro Villa <alessandro17.villa@mail.polimi.it>

--- src/it/polimi/tiw/controllers/Gallery.java
+++ src/it/polimi/tiw/controllers/Gallery.java
@@ -91,7 +91,7 @@ public class Gallery extends HttpServlet {
 
         try{
             campaign = campaignDAO.getCampaignById(campaignId);
-            if (!campaign.getState().equals(Campaign.STARTED)){
+            if (campaign == null || !campaign.getState().equals(Campaign.STARTED)){
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
                 return;
             }
@@ -102,10 +102,6 @@ public class Gallery extends HttpServlet {
             return;
         }
 
-        if(campaign==null){
-            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            return;
-        }
         if(images == null){
             ctx.setVariable("isImageAvailable", false);
             images = new ArrayList<>();
